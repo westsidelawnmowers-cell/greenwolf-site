e3284eec2119638fa6c339ceb8de284a03501ea2
westsidelawnmowers-cell <greenwolfsaskatoon@gmail.com>
2026-04-20 17:06:57 -0600

    up

--- script.js
+++ script.js
@@ -1127,9 +1127,8 @@ function setupAddonSwipeHints() {
 
     const updateHint = () => {
       const panelVisible = !panel.hidden && panel.offsetParent !== null;
-      const hasOverflow = grid.scrollWidth - grid.clientWidth > 16;
       const hasSwiped = grid.scrollLeft > 20;
-      hint.classList.toggle('is-hidden', !mobileQuery.matches || !panelVisible || !hasOverflow || hasSwiped);
+      hint.classList.toggle('is-hidden', !mobileQuery.matches || !panelVisible || hasSwiped);
     };
 
     grid.addEventListener('scroll', updateHint, { passive: true });
